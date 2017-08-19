68ae022555c3517148e77c3340a08cf25babfd83
iamdingzi <465515708@qq.com>
2017-08-20 01:17:58 +0800

    2

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,7 +9,7 @@ function resolve (dir) {
 
 module.exports = {
   entry: {
-    app: './src/main.js'
+    app: '../src/main.js'
   },
   output: {
     path: config.build.assetsRoot,
@@ -50,11 +50,11 @@ module.exports = {
 
         test: /\.less$/,
         use: [{
-            loader: "style-loader" // creates style nodes from JS strings 
+            loader: "style-loader" // creates style nodes from JS strings
         }, {
-            loader: "css-loader" // translates CSS into CommonJS 
+            loader: "css-loader" // translates CSS into CommonJS
         }, {
-            loader: "less-loader" // compiles Less to CSS 
+            loader: "less-loader" // compiles Less to CSS
         }]
       },
       {
